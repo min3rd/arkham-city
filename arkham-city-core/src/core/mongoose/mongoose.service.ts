9240aca98dff4d9d463b9484c145f77e59b4806e
vanminh.vu <min6th@gmail.com>
2025-03-15 14:13:41 +0700

    #4 parse normal object. tree object and array object

--- arkham-city-core/src/core/mongoose/mongoose.service.ts
+++ arkham-city-core/src/core/mongoose/mongoose.service.ts
@@ -1,4 +1,52 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MongooseService {}
+export class MongooseService {
+  fromDataToType(data: object) {
+    if (typeof data !== 'object') {
+      return false;
+    }
+    if (data instanceof Array) {
+      return false;
+    }
+    const dataType = {};
+    for (const key of Object.keys(data)) {
+      if (typeof data[key] === 'string') {
+        dataType[key] = {
+          type: String,
+        };
+        try {
+          const date = Date.parse(`${data[key]}`);
+          if (!isNaN(date)) {
+            dataType[key] = {
+              type: Date,
+            };
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      } else if (typeof data[key] === 'number') {
+        dataType[key] = {
+          type: Number,
+        };
+      } else if (typeof data[key] === 'bigint') {
+        dataType[key] = {
+          type: BigInt,
+        };
+      } else if (typeof data[key] === 'boolean') {
+        dataType[key] = {
+          type: Boolean,
+        };
+      } else if (typeof data[key] === 'object') {
+        if (data[key] instanceof Array) {
+          dataType[key] = {
+            type: Array,
+          };
+        } else {
+          dataType[key] = this.fromDataToType(data[key] as object);
+        }
+      }
+    }
+    return dataType;
+  }
+}
