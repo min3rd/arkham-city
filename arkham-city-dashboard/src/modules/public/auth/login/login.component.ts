7dd78b497ce565516931d272bfe0197844870e60
Vũ Văn Minh <min6th@gmail.com>
2025-03-20 16:51:05 +0700

    #43 add template input with form control handle

--- arkham-city-dashboard/src/modules/public/auth/login/login.component.ts
+++ arkham-city-dashboard/src/modules/public/auth/login/login.component.ts
@@ -1,10 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BaseComponent } from '../../../../core/components/base/base.component';
+import { TextInputComponent } from '../../../../core/components/inputs/text-input/text-input.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, TextInputComponent],
   templateUrl: './login.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent extends BaseComponent {}
+export class LoginComponent extends BaseComponent {
+  override ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: ['email@email.com', [Validators.required]],
+      password: ['ThisIsNewPassword', [Validators.required]],
+    });
+  }
+}
