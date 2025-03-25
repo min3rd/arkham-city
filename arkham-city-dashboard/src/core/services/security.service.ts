c987036aa4727423e3cf1e9f77dec99f0f62c1ba
vanminh.vu <min6th@gmail.com>
2025-03-25 21:12:04 +0700

    #62 store auth into local storage and load on init, remove bcrypt

--- arkham-city-dashboard/src/core/services/security.service.ts
+++ arkham-city-dashboard/src/core/services/security.service.ts
@@ -1,27 +1,18 @@
 import { Injectable } from '@angular/core';
-import { compareSync, genSaltSync, hashSync } from 'bcrypt';
-import { AES } from 'crypto-js';
+import { AES, enc } from 'crypto-js';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SecurityService {
   private readonly PRIVATE_KEY: string = 'Ohhhh shit!!! Here we go again!!!';
-  private readonly SALT_ROUND: number = 69;
   constructor() {}
-  hash<T>(data: T) {
-    const salt = genSaltSync(this.SALT_ROUND);
-    return hashSync(JSON.stringify(data), salt);
-  }
-  compare<T>(target: T, source: string): boolean {
-    return compareSync(JSON.stringify(target), source);
-  }
   encrypt<T>(data: T): string {
     return AES.encrypt(JSON.stringify(data), this.PRIVATE_KEY).toString();
   }
   decrypt<T>(encrypted: string): string | T {
     const decrypted = AES.decrypt(encrypted, this.PRIVATE_KEY).toString(
-      CryptoJS.enc.Utf8
+      enc.Utf8
     );
     try {
       const data = JSON.parse(decrypted);
