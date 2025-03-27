81a505011a962645d04e724bbe86a2e13c75554f
Vũ Văn Minh <min6th@gmail.com>
2025-03-27 15:10:39 +0700

    #73 using new "if" syntax, create new side with preline style

--- arkham-city-dashboard/src/core/layouts/main-layout/main-layout.component.ts
+++ arkham-city-dashboard/src/core/layouts/main-layout/main-layout.component.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ArkButton } from '../../components/buttons/ark-button/ark-button.component';
 import { ArkSwitchTheme } from '../../components/buttons/ark-switch-theme/ark-switch-theme.component';
 
 @Component({
   selector: 'main-layout',
-  imports: [CommonModule, RouterModule, ArkButton, ArkSwitchTheme],
+  imports: [CommonModule, RouterModule, ArkSwitchTheme],
   templateUrl: './main-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
