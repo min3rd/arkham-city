3efb898472d899eac48bded89a68bfdfbfdadfd7
Vũ Văn Minh <min6th@gmail.com>
2025-03-27 09:55:57 +0700

    #73 create temporary main layout component

--- arkham-city-dashboard/src/app/app.routes.ts
+++ arkham-city-dashboard/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { EmptyLayoutComponent } from '../core/layouts/empty-layout/empty-layout.component';
 import { authGuard } from '../core/auth/guards/auth.guard';
 import { noAuthGuard } from '../core/auth/guards/no-auth.guard';
+import { MainLayoutComponent } from '../core/layouts/main-layout/main-layout.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
@@ -15,6 +16,7 @@ export const routes: Routes = [
   {
     path: '',
     canActivateChild: [authGuard],
+    component: MainLayoutComponent,
     loadChildren: () =>
       import('../modules/private/private.routes').then((r) => r.routes),
   },
