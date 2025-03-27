8d9015be09dd93bbe6dc2df0bb432f1b2a6fcf14
Vũ Văn Minh <min6th@gmail.com>
2025-03-27 09:27:16 +0700

    #74 create no-auth guard

--- arkham-city-dashboard/src/app/app.routes.ts
+++ arkham-city-dashboard/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { EmptyLayoutComponent } from '../core/layouts/empty-layout/empty-layout.component';
 import { authGuard } from '../core/auth/guards/auth.guard';
+import { noAuthGuard } from '../core/auth/guards/no-auth.guard';
 
 export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
   {
     path: '',
+    canActivateChild: [noAuthGuard],
     component: EmptyLayoutComponent,
     loadChildren: () =>
       import('../modules/public/public.routes').then((r) => r.routes),
---
+++ arkham-city-dashboard/src/core/auth/guards/no-auth.guard.ts
@@ -0,0 +1,18 @@
+import { inject } from '@angular/core';
+import { Router, type CanActivateChildFn } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { of, switchMap } from 'rxjs';
+
+export const noAuthGuard: CanActivateChildFn = (childRoute, state) => {
+  const router: Router = inject(Router);
+  return inject(AuthService)
+    .check()
+    .pipe(
+      switchMap((authenticated) => {
+        if (authenticated) {
+          return of(router.parseUrl(''));
+        }
+        return of(true);
+      })
+    );
+};
