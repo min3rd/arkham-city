8150a79eaa08cfd6472f5b054cd70db0b087447b
Vũ Văn Minh <min6th@gmail.com>
2025-03-27 09:55:57 +0700

    #73 create temporary main layout component

--- arkham-city-dashboard/src/core/layouts/main-layout/main-layout.component.ts
+++ arkham-city-dashboard/src/core/layouts/main-layout/main-layout.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ArkButton } from '../../components/buttons/ark-button/ark-button.component';
+import { ArkSwitchTheme } from '../../components/buttons/ark-switch-theme/ark-switch-theme.component';
 
 @Component({
   selector: 'main-layout',
-  imports: [],
+  imports: [CommonModule, RouterModule, ArkButton, ArkSwitchTheme],
   templateUrl: './main-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainLayoutComponent implements OnInit {
-
-  ngOnInit(): void { }
-
+  ngOnInit(): void {}
 }
