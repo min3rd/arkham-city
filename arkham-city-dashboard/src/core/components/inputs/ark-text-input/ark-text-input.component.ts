a849dc6ffaf9519dbee78bc9bc3b1e1b01db6b0e
vanminh.vu <min6th@gmail.com>
2025-03-29 13:46:05 +0700

    #86 create new-project page base on figma design

--- arkham-city-dashboard/src/core/components/inputs/ark-text-input/ark-text-input.component.ts
+++ arkham-city-dashboard/src/core/components/inputs/ark-text-input/ark-text-input.component.ts
@@ -1,26 +1,15 @@
 import { CommonModule } from '@angular/common';
 import {
-  AfterViewInit,
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
   Component,
-  ContentChild,
-  inject,
   Input,
-  TemplateRef,
-  ViewChild,
 } from '@angular/core';
 import {
   ControlContainer,
-  FormControl,
-  FormControlName,
   FormGroupDirective,
   FormsModule,
   ReactiveFormsModule,
 } from '@angular/forms';
-import { FormElement } from '../../base/form-element/form-element.component';
-import { LoadingService } from '../../../services/loading/loading.service';
-import { Subject, takeUntil } from 'rxjs';
 import { FormControlElement } from '../../base/form-control-element/form-control-element.component';
 
 @Component({
