80ff444c515ca950ac908f4b6c9a5cce34fde152
vanminh.vu <min6th@gmail.com>
2025-03-29 14:23:58 +0700

    #35 create project schema, add compodoc library to generate documentation

--- arkham-city-core/src/modules/base/base.type.ts
+++ arkham-city-core/src/modules/base/base.type.ts
@@ -1,6 +1,23 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+
 export interface ResponseDto {
   timestamp?: Date;
   error?: boolean;
   errorCode?: any;
   data?: any;
+}
+
+@Schema()
+export class AuditEntity {
+  @Prop()
+  createdBy: string;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  lastModifiedBy: string;
+
+  @Prop()
+  lastModifiedAt: Date;
 }
