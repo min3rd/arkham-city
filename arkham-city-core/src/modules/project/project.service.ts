d6c10e3d80c428c0b10f924229255469e69fb131
vanminh.vu <min6th@gmail.com>
2025-03-29 16:25:46 +0700

    #37 add create new-project, update project information

--- arkham-city-core/src/modules/project/project.service.ts
+++ arkham-city-core/src/modules/project/project.service.ts
@@ -1,4 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Project } from './project.type';
+import { Model } from 'mongoose';
+import {
+  BadMicroserviceResponse,
+  MicroserviceErrorCode,
+  SuccessMicroserviceResponse,
+} from 'src/core/microservice/microservice.type';
+import { User } from '../user/user.type';
 
 @Injectable()
-export class ProjectService {}
+export class ProjectService {
+  constructor(
+    @InjectModel(Project.name, 'metadata') private projectModel: Model<Project>,
+  ) {}
+  async create(user: User, name: string, description: string = '') {
+    const check = await this.projectModel.find({
+      name: name,
+      user: user,
+    });
+    if (check.length > 0) {
+      return new BadMicroserviceResponse(
+        MicroserviceErrorCode.PROJECT_ALREADY_EXIST,
+      );
+    }
+    let project = new this.projectModel({
+      name: name,
+      description: description,
+      user: user,
+    });
+    project = await project.save();
+    return new SuccessMicroserviceResponse({ ...project, user: undefined });
+  }
+
+  async update(projectId: string, name: string, description?: string) {
+    let project = await this.projectModel.findById(projectId);
+    if (!project) {
+      return new BadMicroserviceResponse(
+        MicroserviceErrorCode.INCORRECT_PROJECT_ID,
+      );
+    }
+    if (
+      (await this.projectModel.find({ id: projectId, name: name })).length > 0
+    ) {
+      return new BadMicroserviceResponse(
+        MicroserviceErrorCode.PROJECT_ALREADY_EXIST,
+      );
+    }
+    project.name = name;
+    project.description = description;
+    project = await project.save();
+    return new SuccessMicroserviceResponse({ ...project, user: undefined });
+  }
+}
