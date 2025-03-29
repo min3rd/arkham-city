d9ae11099797fd0cd24d197631f82b7b483fbd3c
vanminh.vu <min6th@gmail.com>
2025-03-29 13:46:05 +0700

    #86 create new-project page base on figma design

--- arkham-city-dashboard/src/modules/private/project/new-project/new-project.component.ts
+++ arkham-city-dashboard/src/modules/private/project/new-project/new-project.component.ts
@@ -1,10 +1,34 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BaseComponent } from '../../../../core/components/base/base.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TranslocoModule } from '@jsverse/transloco';
+import { CapitalizePipe } from '../../../../core/pipe/capitalize.pipe';
+import { ArkTextInput } from '../../../../core/components/inputs/ark-text-input/ark-text-input.component';
+import { ArkButton } from '../../../../core/components/buttons/ark-button/ark-button.component';
+import { ArkTextarea } from '../../../../core/components/textareas/ark-textarea/ark-textarea.component';
 
 @Component({
   selector: 'app-new-project',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TranslocoModule,
+    CapitalizePipe,
+    ArkTextInput,
+    ArkButton,
+    ArkTextarea,
+  ],
   templateUrl: './new-project.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NewProjectComponent extends BaseComponent {}
+export class NewProjectComponent extends BaseComponent {
+  override ngOnInit(): void {
+    super.ngOnInit();
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+    });
+  }
+}
