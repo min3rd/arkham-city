d61dd4394bf754216bcd3af85a8fd78e133416dc
vanminh.vu <min6th@gmail.com>
2025-03-30 21:05:20 +0700

    #95 add create, update, delete app service

--- arkham-city-core/src/modules/project/app/app.service.ts
+++ arkham-city-core/src/modules/project/app/app.service.ts
@@ -1,4 +1,93 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { App, APP_TYPE } from './app.type';
+import { Model } from 'mongoose';
+import { JWTPayload } from 'src/modules/auth/auth.type';
+import {
+  BadMicroserviceResponse,
+  MicroserviceErrorCode,
+} from 'src/core/microservice/microservice.type';
+import { randomBytes, randomUUID } from 'crypto';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+  constructor(
+    @InjectModel(App.name, 'metadata') private readonly appModel: Model<App>,
+  ) {}
+
+  async create(
+    user: JWTPayload,
+    projectId: string,
+    name: string,
+    type: APP_TYPE,
+    callback: string,
+    desctiption: string = '',
+  ) {
+    if (
+      (
+        await this.appModel.find({
+          project: {
+            _id: projectId,
+          },
+        })
+      ).length > 0
+    ) {
+      return new BadMicroserviceResponse(MicroserviceErrorCode.DULICATE_APP);
+    }
+    let app = new this.appModel({
+      project: {
+        _id: projectId,
+      },
+      name: name,
+      type: type,
+      callback: callback,
+      description: desctiption,
+      clientId: randomUUID(),
+      secretKey: randomBytes(48).toString('base64'),
+      user: user,
+    });
+    app = await app.save();
+    return app.toJSON();
+  }
+
+  async update(
+    user: JWTPayload,
+    projectId: string,
+    appId: string,
+    name: string,
+    type: APP_TYPE,
+    callback: string,
+    desctiption: string = '',
+  ) {
+    let app = await this.appModel.findOne({
+      _id: appId,
+      project: {
+        _id: projectId,
+      },
+      user: user,
+    });
+    if (!app) {
+      return new BadMicroserviceResponse(MicroserviceErrorCode.APP_NOT_FOUND);
+    }
+    app.name = name;
+    app.type = type;
+    app.callback = callback;
+    app.description = desctiption;
+    app = await app.save();
+    return app.toJSON();
+  }
+
+  async delete(user: JWTPayload, projectId: string, appId: string) {
+    const app = await this.appModel.findOne({
+      _id: appId,
+      project: {
+        _id: projectId,
+      },
+      user: user,
+    });
+    if (!app) {
+      return new BadMicroserviceResponse(MicroserviceErrorCode.APP_NOT_FOUND);
+    }
+    await app.deleteOne();
+  }
+}
--- arkham-city-core/src/modules/project/app/app.type.ts
+++ arkham-city-core/src/modules/project/app/app.type.ts
@@ -1,13 +1,15 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
 import { Project } from '../project.type';
+import { AuditEntity } from 'src/modules/base/base.type';
+import { User } from 'src/modules/user/user.type';
 
 export enum APP_TYPE {
   WEB_APP = 'WEB_APP',
 }
 
 @Schema()
-export class App {
+export class App extends AuditEntity {
   @Prop()
   name: string;
 
@@ -27,6 +29,22 @@ export class App {
     ref: Project.name,
   })
   project: Project;
+
+  @Prop({
+    unique: true,
+  })
+  clientId: string;
+
+  @Prop({
+    unique: true,
+  })
+  secretKey: string;
+
+  @Prop({
+    type: mongoose.Types.ObjectId,
+    ref: User,
+  })
+  user: User;
 }
 
 export type AppDocument = HydratedDocument<App>;
