d7f0cfcb4f4cab8d6ddad18f2995fa07a0edea8f
Vũ Văn Minh <min6th@gmail.com>
2025-03-31 15:11:25 +0700

    chore: remove temp component, add prettier config

--- arkham-city-dashboard/src/modules/private/dashboard/dashboard.component.ts
+++ arkham-city-dashboard/src/modules/private/dashboard/dashboard.component.ts
@@ -1,26 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import {
   ChangeDetectionStrategy,
   Component,
-  inject,
-  type OnInit,
 } from '@angular/core';
-import { ArkButton } from '../../../core/components/buttons/ark-button/ark-button.component';
-import { ConfigService } from '../../../core/services/config.service';
-import { ArkTextarea } from '../../../core/components/textareas/ark-textarea/ark-textarea.component';
 import { BaseComponent } from '../../../core/components/base/base.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule, ArkButton, ArkTextarea],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardComponent extends BaseComponent {
-  private httpClient: HttpClient = inject(HttpClient);
-  private configService: ConfigService = inject(ConfigService);
-  test() {
-    this.httpClient.get(this.configService.endpoint('/test')).subscribe();
-  }
+
 }
