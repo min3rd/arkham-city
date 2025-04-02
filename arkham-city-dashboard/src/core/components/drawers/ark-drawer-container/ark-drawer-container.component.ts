e28acca18a9836549b5c733b46e994b4a55feb7b
Vũ Văn Minh <min6th@gmail.com>
2025-04-02 16:57:59 +0700

    #112 add temporary drawer components

---
+++ arkham-city-dashboard/src/core/components/drawers/ark-drawer-container/ark-drawer-container.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'ark-drawer-container',
+  imports: [],
+  templateUrl: './ark-drawer-container.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ArkDrawerContainer { }
---
+++ arkham-city-dashboard/src/core/components/drawers/ark-drawer/ark-drawer.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'ark-drawer',
+  imports: [],
+  templateUrl: './ark-drawer.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ArkDrawer { }
