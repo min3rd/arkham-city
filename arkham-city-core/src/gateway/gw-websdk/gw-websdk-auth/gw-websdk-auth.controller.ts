be7e3fb01d5b993077f0e51ec2af4e1a008c4495
Vũ Văn Minh <min6th@gmail.com>
2025-04-16 08:41:47 +0700

    #148 add log-in, register API

--- arkham-city-core/src/gateway/gw-websdk/gw-websdk-auth/gw-websdk-auth.controller.ts
+++ arkham-city-core/src/gateway/gw-websdk/gw-websdk-auth/gw-websdk-auth.controller.ts
@@ -1,13 +1,22 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Inject, Post, Req } from '@nestjs/common';
 import { ClientRedis } from '@nestjs/microservices';
 import { microserviceConfig } from 'src/config/microservice.config';
-import { GwWebSDKAuthReqDto } from './gw-websdk-auth.interface';
+import {
+  GwWebSDKAuthLogInDto,
+  GwWebSDKAuthRegisterDto,
+  GwWebSDKAuthReqDto,
+} from './gw-websdk-auth.interface';
 import { ServiceResponse } from 'src/core/microservice/microservice.types';
 import { firstValueFrom } from 'rxjs';
 import { GatewayController } from 'src/core/gateway/gateway.controller';
 import { Public } from 'src/core/decorators/public';
-import { MsWebSDKAuthReqPayload } from 'src/microservices/ms-websdk/ms-websdk-auth/ms-websdk-auth.interface';
+import {
+  MsWebSDKAuthLogInReqPayload,
+  MsWebSDKAuthRegisterReqPayload,
+  MsWebSDKAuthReqPayload,
+} from 'src/microservices/ms-websdk/ms-websdk-auth/ms-websdk-auth.interface';
+import { REQUEST_FIELDS } from 'src/config/request.config';
+import { SDKJwtPayload } from 'src/modules/websdk/websdk-auth/websdl-auth.interface';
 
 @Controller('websdk/auth')
 export class GwWebsdkAuthController extends GatewayController {
@@ -29,6 +38,46 @@ export class GwWebsdkAuthController extends GatewayController {
     const res: ServiceResponse<any> = await firstValueFrom(
       this.clientProxy.send(
         microserviceConfig.websdk.auth.patterns.authenticate,
+        payload,
+      ),
+    );
+    this.afterCallMicroservice(res);
+    return res.data;
+  }
+
+  @Post('log-in-by-email-and-password')
+  async logInByEmailAndPassword(
+    @Req() request: Request,
+    @Body() dto: GwWebSDKAuthLogInDto,
+  ) {
+    const payload: MsWebSDKAuthLogInReqPayload = {
+      auth: request[REQUEST_FIELDS.auth] as SDKJwtPayload,
+      email: dto.email,
+      password: dto.password,
+    };
+    const res: ServiceResponse<any> = await firstValueFrom(
+      this.clientProxy.send(
+        microserviceConfig.websdk.auth.patterns.logInByEmailAndPassword,
+        payload,
+      ),
+    );
+    this.afterCallMicroservice(res);
+    return res.data;
+  }
+
+  @Post('register-by-email-and-password')
+  async registerByEmailAndPassword(
+    @Req() request: Request,
+    @Body() dto: GwWebSDKAuthRegisterDto,
+  ) {
+    const payload: MsWebSDKAuthRegisterReqPayload = {
+      auth: request[REQUEST_FIELDS.auth] as SDKJwtPayload,
+      email: dto.email,
+      password: dto.password,
+    };
+    const res: ServiceResponse<any> = await firstValueFrom(
+      this.clientProxy.send(
+        microserviceConfig.websdk.auth.patterns.registerByEmailAndPassword,
         payload,
       ),
     );
--- arkham-city-core/src/microservices/ms-websdk/ms-websdk-auth/ms-websdk-auth.interface.ts
+++ arkham-city-core/src/microservices/ms-websdk/ms-websdk-auth/ms-websdk-auth.interface.ts
@@ -1,6 +1,20 @@
+import { SDKJwtPayload } from 'src/modules/websdk/websdk-auth/websdl-auth.interface';
+
 export interface MsWebSDKAuthReqPayload {
   projectId: string;
   appId: string;
   secretKey: string;
   userToken?: string;
+}
+
+export interface MsWebSDKAuthLogInReqPayload {
+  auth: SDKJwtPayload;
+  email: string;
+  password: string;
+}
+
+export interface MsWebSDKAuthRegisterReqPayload {
+  auth: SDKJwtPayload;
+  email: string;
+  password: string;
 }
