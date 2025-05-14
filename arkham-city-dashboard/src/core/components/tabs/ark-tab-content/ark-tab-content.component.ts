97e4a59cf90bfdb26e038388129bebc833090e1a
Vũ Văn Minh <min6th@gmail.com>
2025-05-14 15:29:05 +0700

    #173 fix tab show bugs

--- arkham-city-dashboard/src/core/components/tabs/ark-tab-content/ark-tab-content.component.ts
+++ arkham-city-dashboard/src/core/components/tabs/ark-tab-content/ark-tab-content.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, HostBinding, Input, OnChanges, SimpleChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'ark-tab-content',
-  imports: [CommonModule],
   templateUrl: './ark-tab-content.component.html',
+  encapsulation: ViewEncapsulation.None,
+  standalone: true,
 })
-export class ArkTabContent {
-  show = false;
+export class ArkTabContent implements OnChanges {
+  @Input() show = true;
+
+  @HostBinding('style') get styleList(): any {
+    return {
+      visibility: this.show ? 'visible' : 'hidden',
+    };
+  }
 
   setShow(value: boolean) {
     this.show = value;
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if ('show' in changes) {
+      const currentShow = changes['show'].currentValue;
+      this.setShow(currentShow);
+    }
+  }
+
 }
