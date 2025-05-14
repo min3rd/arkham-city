b8d6acde9c25f1e23dd922b8158b208b9732835f
Vũ Văn Minh <min6th@gmail.com>
2025-05-14 09:19:52 +0700

    #173 fix ark-switch-theme button

--- arkham-city-dashboard/src/core/components/buttons/ark-switch-theme/ark-switch-theme.component.ts
+++ arkham-city-dashboard/src/core/components/buttons/ark-switch-theme/ark-switch-theme.component.ts
@@ -10,11 +10,34 @@ import { featherMoon, featherSun } from '@ng-icons/feather-icons';
   providers: [provideIcons({ featherSun, featherMoon })],
 })
 export class ArkSwitchTheme implements OnInit {
+
+  get isDarkMode() {
+    return localStorage.getItem('theme') === 'dark';
+  }
+
   ngOnInit() {
+
+    if (!localStorage.getItem('theme')) {
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        localStorage.setItem('theme', 'dark');
+      } else {
+        localStorage.setItem('theme', 'light');
+      }
+    }
+
     document.documentElement.classList.toggle(
       'dark',
-      localStorage.getItem('theme') === 'dark' ||
-      (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches),
+      localStorage.getItem('theme') === 'dark',
     );
+  }
+
+  toggle() {
+    if (this.isDarkMode) {
+      localStorage.setItem('theme', 'light');
+      document.documentElement.classList.remove('dark');
+    } else {
+      localStorage.setItem('theme', 'dark');
+      document.documentElement.classList.add('dark');
+    }
   }
 }
