f4d4b128b6a7d7d2a2253fa05efb20843be39e18
Vũ Văn Minh <min6th@gmail.com>
2025-05-16 13:29:39 +0700

    #173 optimized tabs component

--- arkham-city-dashboard/projects/arkhamcity/src/lib/components/tabs/ark-tab-content/ark-tab-content.component.ts
+++ arkham-city-dashboard/projects/arkhamcity/src/lib/components/tabs/ark-tab-content/ark-tab-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, Input, OnChanges, SimpleChanges, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'ark-tab-content',
@@ -6,24 +6,16 @@ import { Component, HostBinding, Input, OnChanges, SimpleChanges, ViewEncapsulat
   encapsulation: ViewEncapsulation.None,
   standalone: true,
 })
-export class ArkTabContent implements OnChanges {
+export class ArkTabContent {
   @Input() show = true;
 
-  @HostBinding('style') get styleList(): any {
+  @HostBinding('style') get styleList(): { visibility: string } {
     return {
       visibility: this.show ? 'visible' : 'hidden',
     };
   }
 
-  setShow(value: boolean) {
+  setShow(value: boolean): void {
     this.show = value;
   }
-
-  ngOnChanges(changes: SimpleChanges) {
-    if ('show' in changes) {
-      const currentShow = changes['show'].currentValue;
-      this.setShow(currentShow);
-    }
-  }
-
 }
