3e3c05417aa172eaf8cbc9bda7a1c8ecc6e29c9c
Vũ Văn Minh <min6th@gmail.com>
2025-05-16 13:16:20 +0700

    #173 fix the user information button

--- arkham-city-dashboard/projects/arkhamcity/src/lib/components/users/ark-user/ark-user.component.ts
+++ arkham-city-dashboard/projects/arkhamcity/src/lib/components/users/ark-user/ark-user.component.ts
@@ -1,15 +1,39 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, HostListener, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslocoModule } from '@jsverse/transloco';
 import { RouterModule } from '@angular/router';
 import { BaseComponent } from '../../base/base/base.component';
+import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'ark-user',
-  imports: [CommonModule, TranslocoModule, RouterModule],
+  imports: [CommonModule, TranslocoModule, RouterModule, NgIcon],
   templateUrl: './ark-user.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ArkUser extends BaseComponent {
   @Input() user!: any | null | undefined;
+  isDropdownOpen = false;
+
+  constructor(private elementRef: ElementRef) {
+    super();
+  }
+
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  @HostListener('keydown.escape')
+  onEscapeKey(): void {
+    this.isDropdownOpen = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    // Only close if the click is outside the component
+    const clickedInside = this.elementRef.nativeElement.contains(event.target as Node);
+    if (!clickedInside) {
+      this.isDropdownOpen = false;
+    }
+  }
 }
