7af2af798d2214c8be8e4fdec23d78eb2ab38412
Vũ Văn Minh <min6th@gmail.com>
2025-05-19 09:10:26 +0700

    #173 fix bug selected value in ark-select

--- arkham-city-dashboard/projects/arkhamcity/src/lib/components/selects/ark-select/ark-select.component.ts
+++ arkham-city-dashboard/projects/arkhamcity/src/lib/components/selects/ark-select/ark-select.component.ts
@@ -1,7 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, ContentChild, EventEmitter, forwardRef, Output, TemplateRef } from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  EventEmitter,
+  forwardRef,
+  OnDestroy,
+  OnInit,
+  Output,
+  TemplateRef,
+} from '@angular/core';
 import { FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 import { FormControlElement } from '../../base/form-control-element/form-control-element.component';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'ark-select',
@@ -15,11 +25,60 @@ import { FormControlElement } from '../../base/form-control-element/form-control
     },
   ],
 })
-export class ArkSelect extends FormControlElement {
+export class ArkSelect extends FormControlElement implements OnInit, OnDestroy {
   @ContentChild('options') options!: TemplateRef<any>;
   @Output() onChange: EventEmitter<any> = new EventEmitter<any>();
 
+  selectedValue: any;
+  private unsubscribe$ = new Subject<void>();
+
+  override ngOnInit() {
+    // Initialize selectedValue from model if provided
+    if (this.model !== undefined && this.model !== null) {
+      this.selectedValue = this.model;
+    }
+
+    // Subscribe to form control value changes
+    if (this.formFieldControl) {
+      this.formFieldControl.valueChanges
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe(value => {
+          this.selectedValue = value;
+        });
+    }
+  }
+
   change(e: Event) {
-    this.onChange.emit((e.target as any).value);
+    this.selectedValue = (e.target as any).value;
+    this.onChange.emit(this.selectedValue);
+  }
+
+  // Implement ControlValueAccessor
+  writeValue(value: any): void {
+    if (value !== undefined && value !== null) {
+      this.selectedValue = value;
+      // Update the form control if it exists
+      if (this.formFieldControl) {
+        this.getFormControl().setValue(value, { emitEvent: false });
+      }
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange.subscribe(fn);
+  }
+
+  registerOnTouched(fn: any): void {
+    // Not implemented
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  override ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+    super.ngOnDestroy();
   }
 }
