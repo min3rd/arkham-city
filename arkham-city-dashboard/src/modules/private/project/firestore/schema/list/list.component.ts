918a0805d0f4f56a7b78d277c0fffc3d1b053eb2
Vũ Văn Minh <min6th@gmail.com>
2025-05-20 16:27:27 +0700

    feat: add pagination controls and update list component for dynamic page handling

--- arkham-city-dashboard/src/modules/private/project/firestore/schema/list/list.component.ts
+++ arkham-city-dashboard/src/modules/private/project/firestore/schema/list/list.component.ts
@@ -40,6 +40,8 @@ import { FormBuilder, ReactiveFormsModule, UntypedFormGroup } from '@angular/for
 export class ListComponent extends BaseListComponent {
   @ViewChild('drawer') drawer!: ArkDrawer;
   pageSchema!: Pagination<SchemaResDto>;
+  page = 1;
+  size = 10;
 
   form!: UntypedFormGroup;
 
@@ -60,6 +62,12 @@ export class ListComponent extends BaseListComponent {
     this.activatedRoute.params.pipe(takeUntil(this.unsubscribeAll)).subscribe(params => {
       if ('query' in params && params['query'] !== 'all') {
         this.form.get('search')?.setValue(params['query']);
+      }
+      if ('page' in params) {
+        this.page = +params['page'];
+      }
+      if ('size' in params) {
+        this.size = +params['size'];
       }
     });
   }
