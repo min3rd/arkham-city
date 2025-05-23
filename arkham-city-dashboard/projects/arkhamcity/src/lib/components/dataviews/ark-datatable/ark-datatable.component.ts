5e0f74564498382de41377defe4fb96fd650d3d9
Vũ Văn Minh <min6th@gmail.com>
2025-05-23 13:56:28 +0700

    feat: implement ark-datatable component with no records message and enhance paginator options

--- arkham-city-dashboard/projects/arkhamcity/src/lib/components/dataviews/ark-datatable/ark-datatable.component.ts
+++ arkham-city-dashboard/projects/arkhamcity/src/lib/components/dataviews/ark-datatable/ark-datatable.component.ts
@@ -1,12 +1,20 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  Input,
+  TemplateRef,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { BaseComponent } from '../../base/base/base.component';
 import { ArkTextInput } from '../../inputs/ark-text-input/ark-text-input.component';
 import { ArkButton } from '../../buttons/ark-button/ark-button.component';
 import { CapitalizePipe } from '../../../pipes/capitalize.pipe';
 import { TranslocoModule } from '@jsverse/transloco';
-import { Pagination } from '../../../type/pagination.types';
 import { CommonModule } from '@angular/common';
 import { ArkPaginator } from '../../paginators/ark-paginator/ark-paginator.component';
+import { CdkListItemDirective } from './cdk-list-item.directive';
 
 @Component({
   selector: 'ark-datatable',
@@ -24,6 +32,21 @@ import { ArkPaginator } from '../../paginators/ark-paginator/ark-paginator.compo
   encapsulation: ViewEncapsulation.None,
   standalone: true,
 })
-export class ArkDatatable extends BaseComponent {
-  @Input() page!: Pagination<any>;
+export class ArkDatatable extends BaseComponent implements AfterContentInit, AfterViewInit {
+  @Input() page!: number;
+  @Input() pageSize!: number;
+  @Input() total!: number;
+  @Input() pageSizeOptions: number[] = [10, 25, 50, 100];
+  @Input() data!: any[];
+  @Input() disablePageSelector: boolean | string = true;
+
+  @ViewChild(CdkListItemDirective) listTemplate !: TemplateRef<any>;
+
+  ngAfterContentInit() {
+    console.log(this.listTemplate);
+  }
+
+  ngAfterViewInit() {
+    console.log(this.listTemplate);
+  }
 }
