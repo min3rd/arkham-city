76a68412be1184b730a87ba9064ea98d5fb531f5
Vũ Văn Minh <min6th@gmail.com>
2025-11-12 11:28:06 +0700

    feat: implement ControlValueAccessor for ark-text-input component and bind ngModelChange

--- arkham-city-dashboard/projects/arkhamcity/src/lib/components/inputs/ark-text-input/ark-text-input.component.ts
+++ arkham-city-dashboard/projects/arkhamcity/src/lib/components/inputs/ark-text-input/ark-text-input.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, forwardRef, Input } from '@angular/core';
-import { FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormsModule,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+  ControlValueAccessor,
+} from '@angular/forms';
 import { FormControlElement } from '../../base/form-control-element/form-control-element.component';
 
 @Component({
@@ -15,6 +20,40 @@ import { FormControlElement } from '../../base/form-control-element/form-control
     },
   ],
 })
-export class ArkTextInput extends FormControlElement {
-  @Input() type!: string;
+export class ArkTextInput extends FormControlElement implements ControlValueAccessor {
+  @Input() type: string = 'text';
+
+  // ControlValueAccessor callbacks
+  private onChange: (value: any) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  // When used with formControlName (i.e. CVA path) we rely on model as the value container
+  writeValue(obj: any): void {
+    this.model = obj;
+    // Trigger change detection if needed
+    this.changeDetectorRef.markForCheck();
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    // Base component already derives disabled state from loading; we can store a flag
+    // This method is optional for now; template uses enableDisabled()
+    // If a dedicated disabled input is added later, wire it here.
+  }
+
+  // Called when internal ngModel (CVA usage branch) changes
+  onInternalModelChange(value: any) {
+    this.onChange(value);
+  }
+
+  onInternalBlur() {
+    this.onTouched();
+  }
 }
